e59dce7a8dc3eceb194f184fbf6fdbbfe7387125
Julião Kataleko <juliofeli78@gmail.com>
2023-06-10 12:00:29 +0100

    feat: math

--- 1-vars.dart
+++ 1-vars.dart
@@ -13,4 +13,27 @@ void main() {
   double weight = 58.5;
   print(age);
   print(weight);
+
+  bool status = true;
+  bool isTrue = 10 > 5;
+  print(status);
+  print(isTrue);
+
+  resultado = status ? "Verdadeiro" : "Falso";
+  print(resultado);
+
+  // o var recebe qualquer tipo de valor
+  var escola = "ISP Paiget de Benguela";
+  var ano = 2023;
+  var terminado = false;
+
+  // o valor so aceita tipos de valor iguais ao que foi inicializado
+  // escola = 200; // erro
+
+  // Dynamic
+  dynamic myAge = 28;
+  myAge = "I dont know";
+  myAge = false;
+  print(myAge);
+  
 }
---
+++ 2-math.dart
@@ -0,0 +1,32 @@
+void main() {
+
+  double num1 = 20;
+  double num2 = 8;
+
+  double sum = num1 + num2;
+  print(sum);
+
+  double minus = num1 - num2;
+  print(minus);
+
+  double times = num1 * num2;
+  print(times);
+
+  double division = num1 / num2;
+  print(division);
+
+  // increment
+  num1 = num1 + 50;
+  num1 += 50;
+  num1++;
+
+  // decrement
+  num1--;
+  print(num1);
+
+  // rest of division
+  num1++;
+  var rest = num1 % 2;
+  print(rest);
+  
+}
